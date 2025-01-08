fee5b920e3b40323dcd52fe39adf39b459d4c26c
igoritos22 <igoritos_22@hotmail.com>
2025-01-08 17:05:19 -0300

    feat: add get all techniques

--- internal/router/router.go
+++ internal/router/router.go
@@ -28,7 +28,7 @@ func NewRouter(fightersController *fighterscontroller.FightersController, techni
 	fighterRoutes := router.Group("/api/fighters")
 	{
 		fighterRoutes.POST("/", fightersController.Create)
-		fighterRoutes.GET("/all", fightersController.FindAll)
+		fighterRoutes.GET("/", fightersController.FindAll)
 		fighterRoutes.GET("/fight/:id1/:id2", fightersController.FightersCompare)
 		fighterRoutes.GET("/:id", fightersController.FindById)
 		fighterRoutes.PATCH("/:id", fightersController.UpdateFighter)
